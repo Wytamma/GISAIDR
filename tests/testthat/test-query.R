795b0a29736e30b7ed5cc6f6454177cd3a13c52d
Wytamma Wirth <wytamma.wirth@me.com>
2021-04-29 14:32:22 +1000

    create getData func

--- tests/testthat/test-query.R
+++ tests/testthat/test-query.R
@@ -4,27 +4,6 @@ credentials <- login(username = username, password = password)
 # will break if log in fails...
 
 test_that("basic query works", {
-  ev <- create_command(
-    wid = credentials$wid,
-    pid = credentials$pid,
-    cid = 'c_qs8mrs_pj',
-    cmd = 'GetData',
-    params = setNames(list(), character(0))
-  ) #hack for empty {}
-
-  json_queue <- list(queue = list(ev))
-  ts = as.character(as.integer(Sys.time()) * 1000)
-  data <-
-    create_url_data(
-      sid = credentials$sid,
-      wid = credentials$wid,
-      pid = credentials$pid,
-      queue = json_queue,
-      ts = ts
-    )
-  res <- httr::GET(paste0(GISAID_URL, '?', data))
-  j = httr::content(res, as = 'parsed')
-  df <- data.frame(do.call(rbind, j$records))
+  df <- getData(credentials=credentials)
   expect_true(is.data.frame(df))
-
 })
