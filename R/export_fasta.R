36853c590163ae94c11e4b813ea9b0ecbfecefd0
Sebastian Duchene <sebastian.duchene6@gmail.com>
2021-07-29 08:19:03 +1000

    fixed but in export_fasta.R

--- R/export_fasta.R
+++ R/export_fasta.R
@@ -1,7 +1,7 @@
 export_fasta <- function(seqs, out_file_name, export_dated_only = T){
     require(lubridate)
+    dates <- round(decimal_date(ymd(seqs$date)), 3)
     if(export_dated_only){
-        dates <- round(decimal_date(ymd(seqs$date)), 3)
         seqs <- seqs[!is.na(dates), ]
     } 
     newnames <- paste(seqs$country, seqs$pangolin_lineage,
