8af36e33cd04327c9d927dd8e94476ea192e66f6
Wytamma Wirth <wytamma.wirth@me.com>
2022-02-16 14:02:50 +1100

    Add formating to export_fasta

--- R/export_fasta.R
+++ R/export_fasta.R
@@ -1,12 +1,17 @@
-export_fasta <- function(seqs, out_file_name, export_dated_only = T){
+export_fasta <- function(seqs, out_file_name, export_dated_only = T, delimiter='@', date_format=NULL){
     require(lubridate)
-    dates <- round(decimal_date(ymd(seqs$date)), 3)
+    if (!is.null(date_format)) {
+        dates <- format(ymd(seqs$date),date_format)
+    } else {
+
+        dates <- round(decimal_date(ymd(seqs$date)), 3)
+    }
     if(export_dated_only){
         seqs <- seqs[!is.na(dates), ]
         dates <- dates[!is.na(dates)]
-    } 
+    }
     newnames <- paste(seqs$country, seqs$pangolin_lineage,
-                      seqs$accession_id, dates, sep = '@')
+                      seqs$accession_id, dates, sep = delimiter)
     cat('', file = out_file_name)
     for(i in 1:nrow(seqs)){
         cat('>', newnames[i], '\n', sep= '', file = out_file_name, append = T)
