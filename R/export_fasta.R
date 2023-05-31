cb8da277fd45f6fd84469bd0e34249220bd56c64
Wytamma Wirth <wytamma.wirth@me.com>
2023-05-31 12:55:58 +1000

    :sparkles: choose columns that are included in the fasta export

--- R/export_fasta.R
+++ R/export_fasta.R
@@ -1,26 +1,34 @@
-export_fasta <- function(seqs, out_file_name, export_dated_only = T, delimiter='@', date_format=NULL){
-    if ('pangolin_lineage' %in% names(seqs)) {
-        require(lubridate)
-        if (!is.null(date_format)) {
-            dates <- format(ymd(seqs$date),date_format)
-        } else {
-
-            dates <- round(decimal_date(ymd(seqs$date)), 3)
-        }
-        if(export_dated_only){
-            seqs <- seqs[!is.na(dates), ]
-            dates <- dates[!is.na(dates)]
-        }
-
-        newnames <- paste(seqs$country, seqs$pangolin_lineage,
-                          seqs$accession_id, dates, sep = delimiter)
-    } else {
-        newnames <- paste(seqs$description, sep = delimiter)
+export_fasta <- function(
+    seqs,
+    out_file_name,
+    export_dated_only = T,
+    delimiter='@',
+    date_format=NULL,
+    columns=c('country', 'pangolin_lineage', 'accession_id', 'date')
+){
+  if (all(columns %in% names(seqs))) {
+    if('date' %in% columns){
+      if (is.null(date_format)) {
+        dates <- round(lubridate::decimal_date(lubridate::ymd(seqs$date)), 3)
+      } else {
+        dates <- format(as.Date(seqs$date), date_format)
+      }
+      if(export_dated_only){
+        seqs <- seqs[!is.na(dates), ]
+        dates <- dates[!is.na(dates)]
+      }
+      seqs$date <- dates
     }
+    newnames <- do.call(paste, c(lapply(columns, function(x) {seqs[,x]}), sep = delimiter))
+  } else {
+    missing_columns <- setdiff(columns, names(seqs))
+    message(sprintf("Missing columns %s. Defaulting to `description`", missing_columns))
+    newnames <- paste(seqs$description, sep = delimiter)
+  }
 
-    cat('', file = out_file_name)
-    for(i in 1:nrow(seqs)){
-        cat('>', newnames[i], '\n', sep= '', file = out_file_name, append = T)
-        cat(seqs$sequence[i], '\n', file = out_file_name, append = T)
-    }
+  cat('', file = out_file_name)
+  for(i in 1:nrow(seqs)){
+    cat('>', newnames[i], '\n', sep= '', file = out_file_name, append = T)
+    cat(seqs$sequence[i], '\n', file = out_file_name, append = T)
+  }
 }
